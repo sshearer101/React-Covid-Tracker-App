2b3ce716ac9d5baa8662efa35107622d68ce8642
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-06-29 18:13:11 -0600

    fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
+import Cards from './components/Cards';
+import CountryPicker from './components/CountryPicker';
+import Chart from './components/Chart';
+import axios from 'axios'
+
 
 function App() {
+
+const url = 'https://covid19.mathdro.id/api'
+const [data, setData] = useState([])
+
+useEffect(() => { 
+fetch(url)
+  .then((res) => res.json())
+  .then((json) => setData(json))
+}, [])
+
+console.log(data)
   return (
     <div className="App">
-    
+      <Cards />
+      <CountryPicker />
+      <Chart />
     </div>
   );
 }
